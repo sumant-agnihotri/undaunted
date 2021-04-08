daec3a13a772b195207e7eabaaa53a771f120a2b
Sumant Agnihotri <sumant.agnihotri@indegene.com>
2021-04-08 18:29:12 +0530

    replaced angular home page

--- frontend/src/app/components/add-tutorial/add-tutorial.component.ts
+++ frontend/src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
 
 @Component({
   selector: 'app-add-tutorial',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-tutorial.component.css']
 })
 export class AddTutorialComponent implements OnInit {
+  tutorial = {
+    title: '',
+    description: '',
+    published: false
+  };
+  submitted = false;
 
-  constructor() { }
+  constructor(private tutorialService: TutorialService) { }
 
   ngOnInit(): void {
+  }
+
+  saveTutorial(): void {
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description
+    };
+
+    this.tutorialService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  newTutorial(): void {
+    this.submitted = false;
+    this.tutorial = {
+      title: '',
+      description: '',
+      published: false
+    };
   }
 
 }
