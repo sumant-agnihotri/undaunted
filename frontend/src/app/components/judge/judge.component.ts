1570d81bae444af6f56166c7d8f0e598894cae22
Sumant Agnihotri <sumant.agnihotri@indegene.com>
2021-04-11 01:58:22 +0530

    sub imgs render dynamically broken imgs are removed added modal for sub imgs to view full size

--- frontend/src/app/components/judge/judge.component.ts
+++ frontend/src/app/components/judge/judge.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+// import * as $ from 'jquery';
 @Component({
   selector: 'app-judge',
   templateUrl: './judge.component.html',
   styleUrls: ['./judge.component.css'],
 })
 export class JudgeComponent implements OnInit {
+  closeResult = '';
   subs: string[];
+  _sub: string;
+  _num: string;
 
-  constructor() {
+  constructor(private modalService: NgbModal) {
     this.subs = [
       'Ancano-Necro',
       'Autumn_Equinox-NB',
@@ -23,6 +27,43 @@ export class JudgeComponent implements OnInit {
       'Zass-Ward',
       'Ziggie-Necro',
     ];
+  }
+
+  // ngAfterViewInit() {
+  //   $('img').each(function () {
+  //     var image = $(this);
+  //     if (
+  //       image.context.naturalWidth == 0 ||
+  //       image.readyState == 'uninitialized'
+  //     ) {
+  //       $(image).unbind('error').hide();
+  //     }
+  //   });
+  // }
+
+  openXl(content, sub, num) {
+    this._sub = sub;
+    this._num = num;
+    this.modalService
+      .open(content, { size: 'xl' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
   ngOnInit(): void {}
